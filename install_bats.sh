8190e87152be94b29d2954d72b096b2e13951a0a
Marc69 <marc.demey@clustervision.com>
2016-08-03 14:34:36 +0200

    Testing clusterbats

--- install_bats.sh
+++ install_bats.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-yum -y install git jq
-git clone https://github.com/sstephenson/bats.git && bats/install.sh /usr/local
+apt-get -y install git jq
+git clone https://github.com/sstephenson/bats.git && bats/install.sh
 pip install python-hostlist
 pip install httpie 
 
