98f23fe4454589dd1260625edc9827bc7ee30068
Marc de Mey <marc@localhost.localdomain>
2016-08-05 11:21:33 +0200

    Modified install_bats.sh

--- install_bats.sh
+++ install_bats.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-apt-get -y install git jq python-pip
 git clone https://github.com/sstephenson/bats.git && bats/install.sh ../.local/bin
 pip install python-hostlist
 pip install httpie 
